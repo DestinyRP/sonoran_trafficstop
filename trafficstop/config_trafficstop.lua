f6684056f830d03d77cdb523e5fca207baf51ab6
Oliver <olipro@uptheinter.net>
2020-11-24 11:08:24 +0000

    Add DestinyRP config.

---
+++ trafficstop/config_trafficstop.lua
@@ -0,0 +1,24 @@
+--[[
+    Sonoran Plugins
+
+    Plugin Configuration
+
+    Put all needed configuration in this file.
+]]
+config = {
+    enabled = true,
+    pluginName = "trafficstop", -- name your plugin here
+    pluginAuthor = "SonoranCAD", -- author
+    requiresPlugins = {"locations"}, -- required plugins for this plugin to work, separated by commas
+
+    -- put your configuration options below
+    origin = 2, -- 0 = CALLER / 1 = RADIO DISPATCH / 2 = OBSERVED / 3 = WALK_UP
+    status = 1, -- 0 = PENDING / 1 = ACTIVE / 2 = CLOSED
+    priority = 1, -- 1, 2, or 3
+    title = "Traffic Stop", -- This is the title of the call by default it is sent as "Traffic Stop"
+    code = "10-38 - Traffic Stop" -- Change this to reflect your communities 10 Code for a Traffic Stop
+}
+
+if config.enabled then
+    Config.RegisterPluginConfig(config.pluginName, config)
+end
